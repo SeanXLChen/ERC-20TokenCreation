fab532874f925992c8fe5cc0045fb6a5692ea864
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-06-01 20:46:32 -0700

    Fix: naming issue of env var

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,11 +7,11 @@ module.exports = {
   networks: {
     sepolia: {
       url: process.env.ALCHEMY_SEPOLIA_URL,
-      accounts: [process.env.SEAN_PRIVATE_KEY],
+      accounts: [process.env.WALLET_PRIVATE_KEY, process.env.WALLET2_PRIVATE_KEY],
     },
     mainnet: {
       url: process.env.ALCHEMY_MAINNET_URL,
-      accounts: [process.env.SEAN_PRIVATE_KEY],
+      accounts: [process.env.WALLET_PRIVATE_KEY],
     },
     localhost: {
       url: "http://127.0.0.1:8545/"
